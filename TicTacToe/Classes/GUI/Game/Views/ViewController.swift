97d28dde46bb40d08c13dbb1006612cfe6c35bb5
2019_DEV_114 <>
2019-02-28 10:58:51 +0200

    Set up GUI structure

--- TicTacToe/Classes/GUI/Game/Views/ViewController.swift
+++ TicTacToe/Classes/GUI/Game/Views/ViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
