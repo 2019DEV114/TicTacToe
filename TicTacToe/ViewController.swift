4f9ed8879e957d3d1fa059ca6f12f235106f47ea
2019_DEV_114 <>
2019-02-28 09:23:49 +0200

    Added default Xcode generated project.

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by 2019_DEV_114 on 2/28/19.
+//  Copyright © 2019 2019_DEV_114. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
