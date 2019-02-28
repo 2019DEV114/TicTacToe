34b277d7944f9ef83ae349cd1f391a3a6decf773
2019_DEV_114 <>
2019-02-28 09:43:20 +0200

    Added basic player mark model.

---
+++ TicTacToe/Classes/Model/PlayerMarkModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerMarkModel.swift
+//  TicTacToe
+//
+//  Created by 2019_DEV_114 on 2/28/19.
+//  Copyright © 2019 2019_DEV_114. All rights reserved.
+
+import Foundation
+
+enum PlayerMarkModel {
+    case x
+    case o
+}
